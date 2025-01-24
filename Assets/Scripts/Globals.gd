1b5b18680414131bc85fa4034fe6632c1164507a
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2025-01-24 20:18:35 +0100

    Moved elements to Assets folder

--- Assets/Scripts/Globals.gd
+++ Assets/Scripts/Globals.gd
@@ -3,11 +3,10 @@ extends Node
 ############SIGNALS
 
 
-
 ###########END_SIGNALS
-
 
 ###########VARIABLES
 var players : Array = []
+var current_player = null
 var SceneManager : SceneManagement = null
 ###########END_VARIABLES
---
+++ Assets/Scripts/InputManager.gd
@@ -0,0 +1,19 @@
+class_name InputManager extends Node
+
+func _input(event):
+	
+	
+	if event.is_action_pressed("jump"):
+		pass
+	elif event.is_action_pressed("up"):
+		pass 
+	elif event.is_action_pressed("down"):
+		pass 
+	elif event.is_action_pressed("left"):
+		pass 
+	elif event.is_action_pressed("right"):
+		pass 
+	elif event.is_action_pressed("drift"):
+		pass 
+	elif event.is_action_pressed("horny"):
+		pass 
