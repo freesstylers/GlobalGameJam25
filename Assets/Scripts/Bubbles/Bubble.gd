0c5f820d9c27332b6838dc02e452487972dc512f
Jose Martin Serrano <josemsdev@gmail.com>
2025-01-25 17:40:27 +0100

    Llamada padre bubble _ready

--- Assets/Scripts/Bubbles/Bubble.gd
+++ Assets/Scripts/Bubbles/Bubble.gd
@@ -4,7 +4,7 @@ extends BaseBall
 var bubbleShader : ShaderMaterial #seteamos los parametros con seet_shader_material
 
 func _ready() -> void:
-	
+	super._ready()
 	#Buscamos el shader y lo metemos en la variable
 	var count = $RigidBody3D/BubbleMesh.get_surface_override_material_count()
 	bubbleShader = $RigidBody3D/BubbleMesh.get_active_material(0) as ShaderMaterial
