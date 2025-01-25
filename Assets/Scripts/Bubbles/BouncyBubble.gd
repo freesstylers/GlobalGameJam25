285822f4981b023adf9a72aab131c7c639c042e8
GCidon <cidonchap@gmail.com>
2025-01-25 13:10:03 +0100

    Pipi

--- Assets/Scripts/Bubbles/BouncyBubble.gd
+++ Assets/Scripts/Bubbles/BouncyBubble.gd
@@ -3,6 +3,9 @@ extends BaseBall
 
 @export var numhits : int = 1
 
+func _ready() -> void:
+	numhits = randi_range(0,2)
+
 func on_hit(body):
 	numhits -= 1
 	if numhits == 0:
--- Assets/Scripts/MesaManager.gd
+++ Assets/Scripts/MesaManager.gd
@@ -8,5 +8,5 @@ func _ready() -> void:
 func spawn_bubbles():
 	for i in spawners.size():
 		var newbubble = GameManager.PoolManager.bubble_prefabs[randi_range(0, GameManager.PoolManager.bubble_prefabs.size()-1)].instantiate() as Node3D
-		newbubble.global_position = spawners[i].global_position
 		add_child(newbubble)
+		newbubble.global_position = spawners[i].global_position
