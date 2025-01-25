f973a519dd099fb4d8fef052aaae3d66382099c3
GCidon <cidonchap@gmail.com>
2025-01-25 15:14:52 +0100

    CHAAAAAAAARGE

--- Assets/Scripts/pruebaplayer.gd
+++ Assets/Scripts/pruebaplayer.gd
@@ -7,19 +7,33 @@ extends Node3D
 var is_moving : bool = false
 var is_my_turn : bool = false
 
+var charging : bool = false
+var charge_meter : float = 0.0
+
 func _ready() -> void:
 	GameManager.PoolManager.StopBall.connect(stop_movement)
+	
+func _process(delta: float) -> void:
+	if charging:
+		charge_meter += delta
+		if charge_meter > 3.0:
+			charge_meter = 3.0
 	
 func _input(event):
 	if not is_moving:
 		var vector = Input.get_vector("down", "up", "right", "left")
 		var vector3 = Vector3(vector.x, 0, -vector.y)
+		
+		if Input.is_action_pressed("Accelerate"):
+			charging = true
 	
 		if Input.is_action_just_released("Accelerate"):
-			rigidbody.apply_force(vector3.normalized() * 500)
+			rigidbody.apply_force(vector3.normalized() * 500 * charge_meter)
 			palo_hit_sound.play()
 			GameManager.PoolManager.HitBall.emit()
 			is_moving = true
+			charging = false
+			charge_meter = 0.0
 	
 func stop_movement():
 	rigidbody.inertia = Vector3.ZERO
