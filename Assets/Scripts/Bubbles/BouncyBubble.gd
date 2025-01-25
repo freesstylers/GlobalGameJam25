4673bc0addbf85da79c99581e0d6c832185a8005
Jose Martin Serrano <josemsdev@gmail.com>
2025-01-25 17:29:39 +0100

    Random seed animacion burbujas

--- Assets/Scripts/Bubbles/BouncyBubble.gd
+++ Assets/Scripts/Bubbles/BouncyBubble.gd
@@ -1,18 +1,14 @@
 class_name BouncyBubble
-extends BaseBall
+extends Bubble
 
 @export var numhits : int = 1
 @export var hitColors : Array[Color] = [Color.AQUA]
 
-var bubbleShader : ShaderMaterial #seteamos los parametros con seet_shader_material
 
 func _ready() -> void:
 	super._ready()
 	numhits = randi_range(0,2)
 	
-	#Buscamos el shader y lo metemos en la variable
-	var count = $RigidBody3D/BubbleMesh.get_surface_override_material_count()
-	bubbleShader = $RigidBody3D/BubbleMesh.get_active_material(0) as ShaderMaterial
 
 func on_hit(body):
 	numhits -= 1
---
+++ Assets/Scripts/Bubbles/Bubble.gd
@@ -0,0 +1,18 @@
+class_name Bubble
+extends BaseBall
+
+var bubbleShader : ShaderMaterial #seteamos los parametros con seet_shader_material
+
+func _ready() -> void:
+	
+	#Buscamos el shader y lo metemos en la variable
+	var count = $RigidBody3D/BubbleMesh.get_surface_override_material_count()
+	bubbleShader = $RigidBody3D/BubbleMesh.get_active_material(0) as ShaderMaterial
+	
+	#Random seed del ruido para que no tengan la misma animacion todas las burbujas
+	var fastNoise : FastNoiseLite = FastNoiseLite.new()
+	fastNoise.seed = randi()
+	bubbleShader.get_shader_parameter("noise").noise = fastNoise
+
+func set_shader_color(color : Color):
+	bubbleShader.set_shader_parameter("bubble_color", Vector4(color.r, color.g, color.b, color.a))
