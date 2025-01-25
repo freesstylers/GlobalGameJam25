816abfde56f0cba6c4f5dbf43ffb997c27389656
Jose Martin Serrano <josemsdev@gmail.com>
2025-01-25 21:03:32 +0100

    Retoques de burbujas

--- Assets/Scripts/Bubbles/BouncyBubble.gd
+++ Assets/Scripts/Bubbles/BouncyBubble.gd
@@ -7,19 +7,18 @@ extends Bubble
 
 func _ready() -> void:
 	super._ready()
-	numhits = randi_range(0,2)
-	set_shader_color(hitColors[numhits])
+	numhits = randi_range(1,2)
+	set_shader_color(hitColors[numhits - 1])
 	
 
 func on_hit(body):
 	numhits -= 1
-	#set_shader_color(hitColors[numhits])
 	if numhits <= 0:
 		GameManager.PoolManager.HitBubble.emit()
 		queue_free()
+	else:
+		set_shader_color(hitColors[numhits - 1])
 
 func set_shader_color(color : Color):
-	var aux_shader : ShaderMaterial = bubbleShader.duplicate(true)
-	aux_shader.set_shader_parameter("bubble_color", Vector4(color.r, color.g, color.b, color.a))
-	bubbleShader = aux_shader.duplicate(true)
+	bubbleShader.set_shader_parameter("bubble_color", Vector4(color.r, color.g, color.b, color.a))
 	
